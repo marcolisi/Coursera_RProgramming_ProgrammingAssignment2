767c2f743c913ebf1e2b858111ec7ae2c1c834b0
Marco Lisi <marco.lisi@hintinnovation.com>
2015-01-22 11:40:12 -0500

    Completion of the assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,165 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The makeCacheMatrix instantiates a list that holds a specified matrix and 
+## functions to set and get the matrix, and holds an inverse of the matrix as 
+## well as to set and get the inverse
 
-## Write a short comment describing this function
+makeCacheMatrix <- 
+    function(
+        theMatrix = matrix()
+    ) 
+    {
+        assertThatTheMatrixIsInvertible(
+            theMatrix
+        )
+        
+        theInverse <- 
+            NULL
+        
+        #---
+        
+        set <-
+            function(
+                theNewMatrix
+            )
+            {
+                theMatrix <<-
+                    theNewMatrix
+                
+                # The cached inverse is invalidated everytime the matrix is set
+                theInverse <<-
+                    NULL
+            }
+        
+        #---
+        
+        get <-
+            function()
+            {
+                theMatrix
+            }
+        
+        #---
+        
+        setInverse <-
+            function(
+                theInverseOfTheMatrix
+            )
+            {
+                theInverse <<-
+                    theInverseOfTheMatrix
+            }
+        
+        #---
+        
+        getInverse <-
+            function()
+            {
+                theInverse
+                
+                # another option would be to calculate and set the inverse
+                # here, instead of needing the call to an external function
+                # if (is.null(theInverse))
+                # {
+                #     theInverse <<-
+                #         solve(
+                #             theMatrix
+                #         )
+                # }
+                # else
+                # {
+                #     message("Getting the cached inverse.")
+                # }
+                #
+                # theInverse
+            }
+        
+        #---
+        
+        theCacheMatrix <-
+            list(
+                set = set,
+                get = get,
+                setInverse = setInverse,
+                getInverse = getInverse
+            )
+        
+        theCacheMatrix
+    }
 
-makeCacheMatrix <- function(x = matrix()) {
 
-}
+## The cacheSolve takes in a cacheMatrix "object" and sets its inverse, if the
+## inverse as not already been calculated
+## --
+## Another option would be: instead of having an external function set the
+## inverse, that the inverse is calculated and cached when the getInverse
+## function is called on the cacheMatrix. Would be much more cohesive design
+## from an OO perspective, considering that the cacheMatrix is a sort of object
+## with getter and setter functions within it.
 
+cacheSolve <-
+    function(
+        theCacheMatrix
+    ) 
+    {
+        theInverse <-
+            theCacheMatrix$getInverse()
+        
+        if (is.null(theInverse))
+        {
+            theMatrix <- 
+                theCacheMatrix$get()
+            
+            theInverse <-
+                solve(
+                    theMatrix
+                )
+            
+            theCacheMatrix$setInverse(
+                theInverse
+            )         
+        }
+        else
+        {
+            message("Getting the cached inverse.")
+        }
+        
+        theInverse
+    }
 
-## Write a short comment describing this function
+#---
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+assertThatTheMatrixIsInvertible <-
+    function(
+        theMatrix
+    )
+    {
+        theNumberOfRowsOfTheMatrix <-
+            nrow(
+                theMatrix
+            )
+        
+        theNumberOfColumnsOfTheMatrix <-
+            ncol(
+                theMatrix
+            )
+        
+        theMatrixIsInvertible <-
+            theNumberOfRowsOfTheMatrix == theNumberOfColumnsOfTheMatrix
+        
+        assert(
+            "The specified matrix is not invertible.",
+            theMatrixIsInvertible
+        )
+    }
+
+assert <-
+    function(
+        message,
+        ...
+    )
+    {
+        if (!all(...))
+        {
+            print(message)
+            stop(...)
+        }
+    }
